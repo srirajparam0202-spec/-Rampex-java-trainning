d294135735692e18d93168b51cdf40e1c9d61a0e
P sriraj <sriraj.param0202@gmail.com>
2026-06-25 15:57:07 +0530

    this file has a code for  forloop and whileloop

---
+++ src/Day_6/classwork/whileloop.java
@@ -0,0 +1,11 @@
+package Day_6.classwork;
+
+public class whileloop {
+    public static void main(String[] args){
+        int i=5;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
