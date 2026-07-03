6dbee09e2c1607f481161bf19891b92f2ac1de49
P sriraj <sriraj.param0202@gmail.com>
2026-07-03 15:20:36 +0530

    this file has a code for  oop

---
+++ src/Day_12/classwork/anagram.java
@@ -0,0 +1,58 @@
+package Day_12.classwork;
+
+public class anagram {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(isAnagram("pot" ,"top"));
+
+        isAnagram("pot", "top");
+
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+
+        //input 2  String arguments
+        //o/p : true or false
+        //steps: 1. check and compare the length of
+        // both strings
+        // 2.if length not same return false ,
+        // else check further
+
+        if (s1.length() == s2.length()) {
+
+            // further check    //s1 =pot
+            // s2 =top
+
+
+            for (int j = 0; j < s2.length(); j++) {
+
+                for (int i = 0; i < s1.length(); i++) {  //top
+
+                    if (s1.charAt(j) == s2.charAt(i)) {
+
+
+                        return true;
+                    }
+
+
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
