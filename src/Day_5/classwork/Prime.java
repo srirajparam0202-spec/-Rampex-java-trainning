cb7ced2ee1eee0493bce41608594fafb9064e298
P sriraj <sriraj.param0202@gmail.com>
2026-07-08 12:08:14 +0530

    this file has a code for  java

---
+++ src/Day_5/classwork/Prime.java
@@ -0,0 +1,20 @@
+package Day_5.classwork;
+
+
+    public class Prime {
+        public static void main(String[] args) {
+            int num = 5;
+            int count = 0;
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+        }
+    }
+
