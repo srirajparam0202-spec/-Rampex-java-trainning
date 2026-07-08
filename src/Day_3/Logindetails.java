bf6b7822f75faece637b18cdf37b82570bb8415f
P sriraj <sriraj.param0202@gmail.com>
2026-07-08 11:56:38 +0530

    this file has a code for  java

---
+++ src/Day_3/Logindetails.java
@@ -0,0 +1,24 @@
+package Day_3;
+
+
+
+import java.util.Scanner;
+    class Logindetails{
+        public static void main(String[]args){
+            String originalUsername = "Divya";
+            int originalpass =3241;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the username:");
+            String username =sc.nextLine();
+            System.out.print("Enter the pass:");
+            int pass = sc.nextInt();
+            if(originalUsername==username&&originalpass==pass){
+                System.out.print("welcome!!");
+            }else{
+                System.out.print("Invalid credentials");
+            }
+        }
+    }
+
+
+
