546d686b4b09bf33fe06672be6dd025eeb7b7265
P sriraj <sriraj.param0202@gmail.com>
2026-07-08 12:00:54 +0530

    this file has a code for  java

---
+++ src/Day_4/Multiply.java
@@ -0,0 +1,17 @@
+package Day_4;
+
+
+import java.util.Scanner;
+    class Multiply{
+        public static void main(String[] args){
+            int multiply=1;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            int n = sc.nextInt();
+            for(int i = 1; i <= n; i++) {
+                multiply = multiply*i;
+                System.out.println(multiply);
+            }
+        }
+    }
+
