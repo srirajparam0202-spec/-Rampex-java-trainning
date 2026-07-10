d1e843ef12282d46b79940c4c2619ef99bef92b0
P sriraj <sriraj.param0202@gmail.com>
2026-07-10 12:21:11 +0530

    this file has a code for array

---
+++ src/Day_11/hometask/AssignmentExpression.java
@@ -0,0 +1,14 @@
+package Day_11.hometask;
+
+
+
+    public class AssignmentExpression {
+        public static void main(String[] args){
+            int x = 5;
+            if (x > 10 && ++x > 5)
+            {
+            }
+            System.out.println(x);
+        }
+    }
+
