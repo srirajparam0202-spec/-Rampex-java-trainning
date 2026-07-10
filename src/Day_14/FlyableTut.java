c90ba3bfa63674897161707f8842008a011f90bb
P sriraj <sriraj.param0202@gmail.com>
2026-07-10 09:58:46 +0530

    this file has a code for array

---
+++ src/Day_14/FlyableTut.java
@@ -0,0 +1,31 @@
+package Day_14;
+
+
+
+    interface Flyable {
+        void fly();
+    }
+    class Bird implements Flyable {
+        @Override
+        public void fly() {
+            System.out.println("Bird is flapping wings");
+        }
+    }
+    class Airplane implements Flyable {
+        @Override
+        public void fly() {
+            System.out.println("Airplane is engaging engines");
+        }
+    }
+    public class FlyableTut {
+        public static void main(String[] args) {
+            Flyable[] flyers = new Flyable[] {
+                    new Bird(),
+                    new Airplane()
+            };
+            for (Flyable flyer : flyers) {
+                flyer.fly();
+            }
+        }
+    }
+
