104fd82f144d1831b1917722dbe829c4a3903a17
P sriraj <sriraj.param0202@gmail.com>
2026-07-10 10:15:05 +0530

    this file has a code for array

---
+++ src/Day_16/TrainCoachManagement.java
@@ -0,0 +1,18 @@
+package Day_16;
+
+
+import java.util.LinkedList;
+
+    public class TrainCoachManagement {
+        public static void main(String[] args) {
+
+            LinkedList<String> train = new LinkedList<>();
+            train.addFirst("Engine");
+            train.addLast("Coach1");
+            train.addLast("Coach2");
+            train.addLast("Guard");
+            train.removeLast();
+            System.out.println(train);
+        }
+    }
+
